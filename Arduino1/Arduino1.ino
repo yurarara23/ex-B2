48ec03d90a6289c20a1283de7d779f8e16ab74a6
yurarara <urauraura2323@gmail.com>
2025-04-24 12:34:07 +0900

    21変更

--- Arduino1/Arduino1.ino
+++ Arduino1/Arduino1.ino
@@ -1,14 +1,17 @@
-int ledPin = 13;
+int RledPin = 9;
+int BledPin = 8;
+int GledPin = 7;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledPin,OUTPUT);
+  pinMode(RledPin,OUTPUT);
+  pinMode(GledPin,OUTPUT);
+  pinMode(BledPin,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin,HIGH);
-  delay(500);
-  digitalWrite(ledPin,LOW);
-  delay(500);
+  digitalWrite(RledPin,HIGH);
+  digitalWrite(GledPin,HIGH);
+  digitalWrite(BledPin,HIGH);
 }

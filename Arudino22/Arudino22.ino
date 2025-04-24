97e8bf2e3d0c77ed90fb802a7f5893f1640cebf5
yurarara23 <urauraura2323@gmail.com>
2025-04-24 15:38:46 +0900

    うまく読み取れない

--- Arudino22/Arudino22.ino
+++ Arudino22/Arudino22.ino
@@ -1,7 +1,7 @@
 const int sensorPin = A0;   
 const int ledPin = 9;       
 
-const int cycleTime = 100;  
+const int cycleTime = 20;  
 
 void setup() {
   Serial.begin(9600);
@@ -10,15 +10,18 @@ void setup() {
 }
 
 void loop() {
-  for (int highTime = 0; highTime <= cycleTime; highTime += 10) {
+  for (int highTime = 0; highTime <= cycleTime; highTime += 2) {
     int lowTime = cycleTime - highTime;
 
     digitalWrite(ledPin, HIGH);
-    delay(highTime);
-    digitalWrite(ledPin, LOW);
-    delay(lowTime);
+    delayMicroseconds(100); // 点灯が安定するまでちょっと待つ
 
-    int brightness = analogRead(sensorPin);
+    int brightness = analogRead(sensorPin); // 点灯中に読む！
+
+    delay(highTime); // 点灯時間キープ
+
+    digitalWrite(ledPin, LOW);
+    delay(lowTime); // 消灯時間
 
     // CSV出力
     Serial.print(highTime);
@@ -28,5 +31,5 @@ void loop() {
     Serial.println(brightness);
   }
 
-  delay(1000); 
+  delay(500); 
 }
